f0a23c26fa0776d8295ca5e0d891dcbe7c8f62dc
chorn <ipz223_chvi@student.ztu.edu.ua>
2024-02-08 15:44:49 +0200

    Added .gitignore and implemented sum calculation

---
+++ Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double sum = num1 + num2;
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
